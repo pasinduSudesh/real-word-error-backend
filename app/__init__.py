09d16e4859eef1b667d784cf90afe0cafd708e9c
Pasindu Sudesh <47638473+pasinduSudesh@users.noreply.github.com>
2022-03-29 00:49:12 +0530

    CORS issue

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
-
+# Added
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-
+# Added
+cors = CORS(app)
 
 if app.config["ENV"] == "production":
 
@@ -16,5 +18,9 @@ else:
 
     app.config.from_object("config.ProductionConfig")
 
+#Added
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 from app import views
+# https://stackoverflow.com/questions/25594893/how-to-enable-cors-in-flask
